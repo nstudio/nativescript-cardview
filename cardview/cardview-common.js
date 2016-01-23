27654cab8f7445bba912feb9bd927d2715a97408
bradmartin <bradmartin0905@gmail.com>
2016-01-23 13:52:22 -0600

    trying to mimic stack-layout.js from {N} files

--- cardview/cardview-common.js
+++ cardview/cardview-common.js
@@ -1,42 +1,52 @@
+var layouts = require("ui/layouts/layout-base");
 var view = require("ui/core/view");
 var color = require("color");
 
 var CardView = (function (_super) {
-
-    global.__extends(CardView, _super);
-
+    __extends(CardView, _super);
     function CardView() {
-        _super.call(this);
+        _super.call(this, arguments);
     }
-
     Object.defineProperty(CardView.prototype, "backgroundColor", {
         get: function () {
-            return this.backgroundColorProperty;
+            return this._getValue(CardView.backgroundColorProperty);
         },
-        set: function(value){
-            this.backgroundColorProperty = new color.Color(value);
+        set: function (value) {
+            this._setValue(CardView.backgroundColorProperty, new color.Color(value));
         }
     });
-
-     Object.defineProperty(CardView.prototype, "radius", {
+    Object.defineProperty(CardView.prototype, "radius", {
         get: function () {
-            return this.radiusProperty;
+            return this._getValue(CardView.radiusProperty);
         },
-        set: function(value){
-            this.radiusProperty = value;
+        set: function (value) {
+            this._setValue(CardView.radiusProperty, value);
         }
     });
-    
-     Object.defineProperty(CardView.prototype, "elevation", {
+    Object.defineProperty(CardView.prototype, "elevation", {
         get: function () {
-            return this.elevationProperty;
+            return this._getValue(CardView.elevationProperty);
         },
-        set: function(value){
-            this.elevationProperty = value;
+        set: function (value) {
+            this._setValue(CardView.elevationProperty, value);
         }
     });
-
+    Object.defineProperty(CardView.prototype, "padding", {
+        get: function () {
+            return this._getValue(CardView.paddingProperty);
+        },
+        set: function (value) {
+            this._setValue(CardView.paddingProperty, value);
+        }
+    });
+    Object.defineProperty(CardView.prototype, "contentPadding", {
+        get: function () {
+            return this._getValue(CardView.contentPaddingProperty);
+        },
+        set: function (value) {
+            this._setValue(CardView.contentPaddingProperty, value);
+        }
+    });
     return CardView;
-})(view.View);
-
+})(layouts.LayoutBase);
 exports.CardView = CardView;
--- cardview/cardview.android.js
+++ cardview/cardview.android.js
@@ -6,45 +6,34 @@
  *************************************************************************************/
 
 var common = require("./cardview-common");
-var utils = require("utils/utils");
-
-require("utils/module-merge").merge(common, module.exports);
-
+global.moduleMerge(common, exports);
 var CardView = (function (_super) {
-
-    global.__extends(CardView, _super);
-
+    __extends(CardView, _super);
     function CardView() {
         _super.apply(this, arguments);
     }
-
-
-    CardView.prototype._createUI = function () {
-
-        this._android = new android.support.v7.widget.CardView(this._context);
-
-        if (this.radius)
-            this._android.setRadius(this.radius);
-
-        if (this.backgroundColor)
-            this._android.setCardBackgroundColor(this.backgroundColor.android);
-
-        if (this.elevation) 
-            this._android.setMaxCardElevation(this.elevation);
-
-
-    };
-
     Object.defineProperty(CardView.prototype, "android", {
         get: function () {
-            return this._android;
-        }
+            return this._layout;
+        },
+        enumerable: true,
+        configurable: true
     });
-
+    CardView.prototype._createUI = function () {
+        this._layout = new android.support.v7.widget.CardView(this._context);
+        if (this.radius)
+            this._layout.setRadius(this.radius);
+        if (this.backgroundColor)
+            this._layout.setCardBackgroundColor(this.backgroundColor.android);
+        if (this.elevation)
+            this._layout.setMaxCardElevation(this.elevation);
+        if (this.padding)
+            this._layout.setContentPadding(this.padding);
+        if (this.contentPadding)
+            this._layout.setPadding(this.contentPadding);
+    };
     return CardView;
-
-})(CardView);
-
+})(common.CardView);
 exports.CardView = CardView;
 
 
