80c867b8d725a121c40792fd33f2fa438ac90547
Nathan Walker <walkerrunpdx@gmail.com>
2016-04-14 20:16:59 -0700

    refactor: ts simplify

--- cardview.ios.ts
+++ cardview.ios.ts
@@ -1,20 +1,17 @@
-import common = require("./cardview-common");
+import {ContentView} from 'ui/content-view';
 import {Color} from "color";
-import uiUtils = require("ui/utils");
-import {PropertyMetadata} from "ui/core/proxy";
-import {Property, PropertyMetadataSettings} from "ui/core/dependency-observable";
-
-global.moduleMerge(common, exports);
+import * as uiUtils from 'ui/utils';
+import {PropertyMetadata} from 'ui/core/proxy';
+import {Property, PropertyMetadataSettings} from 'ui/core/dependency-observable';
 
 declare var MaterialCard: any, UIApplication: any, CGRectMake: any;
 
-export class CardView extends common.CardView {
+export class CardView extends ContentView {
   private _ios: MaterialCard;
 
   constructor() {
     super();
-
-        
+ 
     this._ios = new MaterialCard(CGRectMake(10, uiUtils.ios.getStatusBarHeight() + 10, UIApplication.sharedApplication().statusBarFrame.size.width - 20, 0));
 
     // Default values for MaterialCard    
@@ -24,12 +21,8 @@ export class CardView extends common.CardView {
     // shadowColor = new Color('#000').ios
     // shadowOpacity = 0.4;
     
-    console.log(`CardView constructor ------`);
-    console.log(this._ios);   
-  }
-
-  get ios(): MaterialCard {
-    return this._ios;
+    // console.log(`CardView constructor ------`);
+    // console.log(this._ios);   
   }
 
   get _nativeView(): MaterialCard {
--- demo/app/main-page.ts
+++ demo/app/main-page.ts
@@ -1,74 +1,10 @@
-﻿import observable = require("data/observable");
-import pages = require("ui/page");
-import btnModule = require("ui/button");
-import frame = require("ui/frame");
-import app = require("application");
+﻿import * as app from 'application';
+import {Color} from 'color';
+import * as platform from 'platform';
+import {Demo} from "./main-view-model";
 
-var viewModel = new observable.Observable({});
-
-export function pageLoaded(args: observable.EventData) {
-    var page = <pages.Page>args.object;
-    // var sl = frame.topmost().currentPage.getViewById('stack');
-    // console.log('sl = ' + sl);
-    page.bindingContext = viewModel;
+function pageLoaded(args) {
+  var page = args.object;
+  page.bindingContext = new Demo();
 }
-
-export function goAway(args) {
-    var page = frame.topmost().currentPage;
-    var card = page.getViewById('batCard');
-    console.log(card);
-    card.animate({ 
-        scale: { x: 0, y: 0 },
-        opacity: 0,
-        duration: 1000
-    }).then(() => { 
-      card.visibility = 'collapsed'; 
-    });
-}
-
-export function goAwayJoker(args) {
-    var page = frame.topmost().currentPage;
-    var card = page.getViewById('jokerCard');
-    console.log(card);
-    card.animate({
-        scale: { x: 0, y: 0 },
-        opacity: 0,
-        duration: 1000
-    }).then(() => { 
-      card.visibility = 'collapsed'; 
-    });
-} 
-
-export function cardLoaded(args: observable.EventData) {
-    try {
-        var card = args.object;
-        console.log('card = ' + card);
-        //console.log('card id = ' + card.id);
-        if (app.android) {
-          var androidCard = card.android;
-          console.log('card native android = ' + androidCard);
-          //console.log('radius = ' + card.radius);
-          //console.log('elevation = ' + card.elevation);
-          //console.log('padding = ' + card.padding);
-          //console.log('card background = ' + card.backgroundColor.android);  
-          //var btn = new android.widget.Button(app.android.currentContext);
-          //btn.setText('Hello Baby!'); 
-          //androidCard.addView(btn);
-        } else if (app.ios) {
-          var iosCard = card.ios;
-          console.log('card native ios = ' + iosCard);
-          console.log(iosCard);
-        } 
-        
-        // var sl = frame.topmost().currentPage.getViewById('stack');
-        // console.log('sl = ' + sl);
-         
-    } catch (error) {
-        console.log(error);
-    }
-}
-
-export function getstack(args) {
-    var sl = frame.topmost().currentPage.getViewById('stack');
-    console.log('sl = ' + sl);
-}
+exports.pageLoaded = pageLoaded;
