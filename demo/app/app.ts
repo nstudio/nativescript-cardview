9f5b9893e61a46e1c420b8729c60fedfa64428a7
bradmartin <bradwaynemartin@gmail.com>
2017-01-25 21:52:58 -0600

    fix demo app crash

--- demo/app/app.ts
+++ demo/app/app.ts
@@ -1,4 +1,14 @@
+/*
+In NativeScript, the app.ts file is the entry point to your application.
+You can use this file to perform app-level initialization, but the primary
+purpose of the file is to pass control to the app’s first module.
+*/
+
 import * as app from 'application';
-app.mainModule = "main-page";
-app.cssFile = "./app.css";
-app.start();
+
+app.start({ moduleName: 'main-page' });
+
+/*
+Do not place any code after the application has been started as it will not
+be executed on iOS.
+*/
