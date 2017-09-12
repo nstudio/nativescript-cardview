f7bcf28a3f30efc0d778bb4e5bdfa4cbf65261b7
Brad Martin <bradwaynemartin@gmail.com>
2017-09-12 00:58:29 -0500

    update demo, imports

--- demo/app/app.ts
+++ demo/app/app.ts
@@ -1,14 +1,3 @@
-/*
- In NativeScript, the app.ts file is the entry point to your application.
- You can use this file to perform app-level initialization, but the primary
- purpose of the file is to pass control to the app’s first module.
- */
+import * as app from "tns-core-modules/application";
 
-import * as app from "application";
-
-app.start({ moduleName: 'main-page' });
-
-/*
- Do not place any code after the application has been started as it will not
- be executed on iOS.
- */
+app.start({ moduleName: "main-page" });
--- demo/app/main-view-model.ts
+++ demo/app/main-view-model.ts
@@ -1,45 +1,48 @@
-import { EventData, Observable } from "data/observable";
+import { EventData, Observable } from "tns-core-modules/data/observable";
 import { CardView } from "nativescript-cardview";
-import { isAndroid, isIOS } from "platform";
-import * as frame from "ui/frame";
+import { isAndroid, isIOS } from "tns-core-modules/platform";
+import * as frame from "tns-core-modules/ui/frame";
 
 export class Demo extends Observable {
-
   constructor() {
     super();
   }
 
   public goAway(args) {
-    let page = frame.topmost().currentPage;
-    let card = <CardView>page.getViewById('batCard');
-    card.animate({
-      scale: { x: 0, y: 0 },
-      opacity: 0,
-      duration: 1000
-    }).then(() => {
-      card.visibility = 'collapse';
-    });
+    const page = frame.topmost().currentPage;
+    const card = <CardView>page.getViewById("batCard");
+    card
+      .animate({
+        scale: { x: 0, y: 0 },
+        opacity: 0,
+        duration: 1000
+      })
+      .then(() => {
+        card.visibility = "collapse";
+      });
   }
 
   public goAwayJoker(args) {
-    let page = frame.topmost().currentPage;
-    let card = <CardView>page.getViewById('jokerCard');
-    card.animate({
-      scale: { x: 0, y: 0 },
-      opacity: 0,
-      duration: 1000
-    }).then(() => {
-      card.visibility = 'collapse';
-    });
+    const page = frame.topmost().currentPage;
+    const card = <CardView>page.getViewById("jokerCard");
+    card
+      .animate({
+        scale: { x: 0, y: 0 },
+        opacity: 0,
+        duration: 1000
+      })
+      .then(() => {
+        card.visibility = "collapse";
+      });
   }
 
   public cardLoaded(args: EventData) {
-    let card = <CardView>args.object;
-    console.log('card = ' + card);
+    const card = <CardView>args.object;
+    console.log("card = " + card);
     if (isAndroid) {
-      console.log('card native android = ' + card.android);
+      console.log("card native android = " + card.android);
     } else if (isIOS) {
-      console.log('card native ios = ' + card.ios);
+      console.log("card native ios = " + card.ios);
     }
   }
 }
